392bc2ddbcb36f1120d6110516d3cfd85efb9a08
NattapholSri <s6101012630097@email.kmutnb.ac.th>
2021-08-20 10:19:48 +0700

    init commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  tt_text;
+  new_attr = 'My first app';
+  class_arr = [1,2,3,4]
+  jsonThings = {
+    first: 'value',
+    second: 'obj'
+  }
+  jsonStr = JSON.stringify(this.jsonThings);
   constructor() {}
+  sayHello() {
+    let jsonObj = {
+      name: 'Nattapon',
+      lastname: 'Srijanngam',
+      age: 21
+    };
 
+    let first_array = ['a','b'];
+    
+    console.log(first_array);
+    console.log(jsonObj.name + ' ' + jsonObj.lastname);
+    alert('Hello!!')
+
+  }
+  
+  sayHi() {
+    let appName = 'Test App';
+    alert('Hi!')
+  }
 }
